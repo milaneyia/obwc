0b4c05c7873dc04910320e3dd52b347452438ff3
milaneyia <39941577+milaneyia@users.noreply.github.com>
2021-04-07 00:47:03 -0400

    twitter

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,6 +4,17 @@
         <p class="fs-5 col-md-8">
             something something
         </p>
+
+        <a
+            class="twitter-timeline"
+            data-width="500"
+            data-height="500"
+            data-dnt="true"
+            data-theme="dark"
+            href="https://twitter.com/osubwc?ref_src=twsrc%5Etfw"
+        >
+            Tweets by osubwc
+        </a>
     </div>
 </template>
 
