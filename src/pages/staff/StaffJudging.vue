f8ac9246ecaf2e48373779f39e7df9023ca076df
milaneyia <alvaroa159@gmail.com>
2021-06-13 17:07:07 -0400

    make staff page work

--- src/pages/staff/StaffJudging.vue
+++ src/pages/staff/StaffJudging.vue
@@ -5,16 +5,40 @@
                 Judging List
             </div>
             <div class="card-subtitle">
-                Summary of all the judging done
+                Summary of all the judging done, for detailed scoring go to the result page
+                <router-link :to="{ name: 'results', params: { id: $route.params.id } }">
+                    here
+                </router-link>
             </div>
         </div>
 
         <div class="card my-2">
-            <data-table
+            <div
                 v-if="resultsData?.round.submissions.length"
-                :fields="fields"
-                :items="resultsData.round.submissions"
-            />
+                class="card-body"
+            >
+                <div class="mb-2">
+                    Type:
+                    <a
+                        class="me-2"
+                        href="#"
+                        @click.prevent="judgingType = 1"
+                    >
+                        mapper
+                    </a>
+                    <a
+                        href="#"
+                        @click.prevent="judgingType = 2"
+                    >
+                        player
+                    </a>
+                </div>
+
+                <data-table
+                    :fields="fields"
+                    :items="resultsData.round.submissions"
+                />
+            </div>
 
             <div
                 v-else
@@ -29,7 +53,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { Results } from '../../../shared/integration';
-import { Judging, Team } from '../../../shared/models';
+import { Judging, JUDGING_TYPE, Team, User } from '../../../shared/models';
 import DataTable, { Field } from '../../components/DataTable.vue';
 
 export default defineComponent({
@@ -41,18 +65,31 @@ export default defineComponent({
 
     data () {
         return {
+            judgingType: JUDGING_TYPE.Mappers,
             resultsData: null as Results | null,
             fields: [
                 { key: 'team', label: 'Team', formatter: (team: Team) => team.name },
                 { key: 'judging', label: 'Count', formatter: this.getJudgesInvolvedCount },
                 { key: 'judging', label: 'Judges', formatter: this.getJudgesInvolved },
+                { key: 'judging', label: 'Missing Judges', formatter: this.getMissingJudges },
             ] as Field[],
         };
     },
 
-    async created (): Promise<void> {
-        const { data } = await this.$http.get<Results>(`/api/rounds/${this.$route.params.id}/results`);
-        this.resultsData = data;
+    computed: {
+        allJudges (): User[] {
+            return this.resultsData?.judges || [];
+        },
+    },
+
+    watch: {
+        judgingType: {
+            async handler (type: JUDGING_TYPE): Promise<void> {
+                const { data } = await this.$http.get<Results>(`/api/rounds/${this.$route.params.id}/results?type=${type}`);
+                this.resultsData = data;
+            },
+            immediate: true,
+        },
     },
 
     methods: {
@@ -62,6 +99,12 @@ export default defineComponent({
 
         getJudgesInvolved (judging: Judging[]): string {
             return judging.map(j => j.judge.username).join(', ');
+        },
+
+        getMissingJudges (judging: Judging[]): string {
+            return this.allJudges.filter(judge => judging.every(j => j.judge.id !== judge.id))
+                .map(j => j.username)
+                .join(', ');
         },
     },
 });
