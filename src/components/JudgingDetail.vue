78a2093d52e83643798cbcce23e92c9feff6fa40
milaneyia <alvaroa159@gmail.com>
2021-06-20 18:33:00 -0400

    add general comment to detail

--- src/components/JudgingDetail.vue
+++ src/components/JudgingDetail.vue
@@ -26,32 +26,46 @@
                     >
                         <b>{{ judging.judge.username }}</b>
 
+                        <div class="my-1">
+                            <p class="text-light ms-3">
+                                <span class="text-preline">
+                                    {{ judging.comment }}
+                                </span>
+                            </p>
+                        </div>
+
                         <div
                             v-for="judgingToCriterias in judging.judgingToCriterias"
                             :key="judgingToCriterias.id"
                             class="my-1"
                         >
-                            <a
-                                data-bs-toggle="collapse"
-                                :href="`#judgingToCriteria${judgingToCriterias.id}`"
-                                @click="showComment(judgingToCriterias.id)"
-                            >
-                                <small>
-                                    <i
-                                        class="fas me-2"
-                                        :class="getCollapseClass(judgingToCriterias.id)"
-                                    />
-                                </small>
-                                {{ judgingToCriterias.criteria.name }}
-                                <b>({{ judgingToCriterias.score }})</b>:
-                            </a>
+                            <template v-if="judgingToCriterias.comment">
+                                <a
+                                    data-bs-toggle="collapse"
+                                    :href="`#judgingToCriteria${judgingToCriterias.id}`"
+                                    @click="showComment(judgingToCriterias.id)"
+                                >
+                                    <small>
+                                        <i
+                                            class="fas me-2"
+                                            :class="getCollapseClass(judgingToCriterias.id)"
+                                        />
+                                    </small>
+                                    {{ judgingToCriterias.criteria.name }}
+                                    <b>({{ judgingToCriterias.score }})</b>:
+                                </a>
 
-                            <p
-                                :id="`judgingToCriteria${judgingToCriterias.id}`"
-                                class="text-light ms-3 collapse"
-                            >
-                                <span style="white-space: pre-line;">{{ judgingToCriterias.comment }}</span>
-                            </p>
+                                <p
+                                    :id="`judgingToCriteria${judgingToCriterias.id}`"
+                                    class="text-light ms-3 collapse"
+                                >
+                                    <span class="text-preline">{{ judgingToCriterias.comment }}</span>
+                                </p>
+                            </template>
+                            <template v-else>
+                                {{ judgingToCriterias.criteria.name }}
+                                <b>({{ judgingToCriterias.score }})</b>
+                            </template>
                         </div>
 
                         <hr v-if="i < submission.judging.length - 1">
@@ -98,3 +112,11 @@ export default defineComponent({
     },
 });
 </script>
+
+<style lang="scss">
+
+.text-preline {
+    white-space: pre-line;
+}
+
+</style>
